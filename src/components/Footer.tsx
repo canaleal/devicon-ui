60e50873c80f1288da17f942ebd97ff053fc247f
Alex Canales <alexcanales766@gmail.com>
2023-10-07 00:05:54 -0400

    Removed contact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,7 @@ export const Footer = () => {
         <div className='flex flex-row gap-16  px-32 py-16 bg-zinc-900 text-white'>
 
             <div className='flex-1'>
-                <p className="text-subtitle">Contact</p>
-                <p className="mt-4 text-sm w-4/5">If you have any legal concerns regarding copyrights or want to report an abuse, please reach out to us at <a className="hover:text-green-500 text-green-400" href="mailto:info@devicon.dev">info@devicon.dev</a> Any code/logo contributions should be made through our GitHub repository listed above.</p>
-
-                <p className="text-subtitle mt-8">Disclaimer</p>
+                <p className="text-subtitle">Disclaimer</p>
                 <p className="mt-4 text-sm w-4/5">All product names, logos, and brandsare property of their respective owners. All company, product and service names used in this website are for identification purposes only. Usage of these names, logos, and brands does not imply endorsement of Devicon or its members. All icons/SVGs in this project are not monetized in anyway. It is up to the user to use the logo properly according to the company/group's brand policy. Usage of this site or any icons/SVGs from Devicon means acknowledgement of these conditions.</p>
             </div>
 
