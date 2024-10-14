55b0caa6bef1641cf735212e11180e7c470a4132
Alex Canales <alexcanales766@gmail.com>
2024-10-14 04:17:51 -0400

    Added checkered bg

--- src/features/dashboard/modal/widgets/iconImage/IconImage.tsx
+++ src/features/dashboard/modal/widgets/iconImage/IconImage.tsx
@@ -7,86 +7,85 @@ import { useIconSettingStore } from '../../store/iconSettingStore'
 import { createIconCodeBlockText } from '../../helpers/codeBlockContent'
 import { downloadText } from '../../../../../helpers/downloadText'
 
-
 interface IconContainerProps {
   icon: IIcon
   extraClasses?: string
 }
 
 export const IconImage = ({ icon, extraClasses }: IconContainerProps) => {
+  const { iconSettings } = useIconSettingStore()
 
-  const {
-    iconSettings
-  } = useIconSettingStore()
-
+  const [isCheckeredBackground, setIsCheckeredBackground] = useState<boolean>(false)
   const [isDarkBackground, setIsDarkBackground] = useState<boolean>(false)
   const [isViewBoxVisible, setIsViewBoxVisible] = useState<boolean>(false)
   const [styleMap, setStyleMap] = useState<React.CSSProperties | null>(null)
 
-
   useEffect(() => {
     const tempStyleMap = createStyleMap(icon, iconSettings)
     setStyleMap(tempStyleMap)
-  }, [iconSettings])
+  }, [icon, iconSettings])
 
   const handleToggleBackground = () => {
     setIsDarkBackground(!isDarkBackground)
+    if (isCheckeredBackground) setIsCheckeredBackground(false)
   }
 
   const handleToggleViewBox = () => {
     setIsViewBoxVisible(!isViewBoxVisible)
   }
 
+  const handleCheckeredBackground = () => {
+    setIsCheckeredBackground(!isCheckeredBackground)
+    if (!isCheckeredBackground) setIsDarkBackground(false) // force light theme
+  }
+
   const handleCopySVGClick = async () => {
-    const code = await createIconCodeBlockText(icon, iconSettings, 'SVG');
+    const code = await createIconCodeBlockText(icon, iconSettings, 'SVG')
     const fileName = `${icon.name}-${iconSettings.selectedVersion}-${iconSettings.selectedIconSize.width}x${iconSettings.selectedIconSize.height}.svg`
     downloadText(code, fileName)
   }
 
   return (
-    <div className={`image-container ${isDarkBackground ? 'image-container--dark' : 'image-container--light'} ${extraClasses}`}>
-
-      <div className='image-container__options-list'>
-        <Tooltip content='Download SVG' position='top' flashMessage='Copied!'>
-          <button onClick={() => handleCopySVGClick()} className='button--icon icon--xxl'>
-            <i className='fa-solid fa-download'></i>
-          </button>
-        </Tooltip>
+    <div className={`image-container ${isDarkBackground ? 'image-container--dark' : 'image-container--light'} ${isCheckeredBackground ? 'image-container--checkered' : ''} ${extraClasses}`}>
+      <div className={`image-container__view-box ${isViewBoxVisible ? (isDarkBackground ? 'image-container__view-box--dark' : 'image-container__view-box--light') : ''}`}>
+        {styleMap ? (
+          <div
+            className="image-container__image"
+            style={{
+              height: iconSettings.selectedIconSize.height,
+              width: iconSettings.selectedIconSize.width,
+              ...styleMap
+            }}
+          />
+        ) : (
+          <img
+            className="image-container__image"
+            height={iconSettings.selectedIconSize.height}
+            width={iconSettings.selectedIconSize.width}
+            src={iconSettings.iconUrl}
+          />
+        )}
       </div>
 
-   
-        <div className={`image-container__view-box  ${isViewBoxVisible ? (isDarkBackground ? 'image-container__view-box--dark' : 'image-container__view-box--light') : ''}`}>
-          {styleMap ? (
-            <div
-              className='image-container__image'
-              style={{
-                height: iconSettings.selectedIconSize.height,
-                width: iconSettings.selectedIconSize.width,
-                ...styleMap
-              }}
-            />
-          ) : (
-            <img
-              className='image-container__image'
-              height={iconSettings.selectedIconSize.height}
-              width={iconSettings.selectedIconSize.width}
-              src={iconSettings.iconUrl}
-            />
-          )}
-        </div>
- 
-
-
-      <div className='image-container__options-list'>
-        <Tooltip content={`${isDarkBackground ? 'Light' : 'Dark'} Background`} position='top'>
-          <button onClick={handleToggleBackground} className='button--icon icon--xxl'>
-            {isDarkBackground ? <i className='fa-solid fa-sun'></i> : <i className='fa-solid fa-moon'></i>}
+      <div className="image-container__options-list">
+        <Tooltip content="Download SVG" position="top" flashMessage="Copied!">
+          <button onClick={handleCopySVGClick} className="button--icon icon--xxl">
+            <i className="fa-solid fa-download"></i>
           </button>
         </Tooltip>
-
-        <Tooltip content={`${isViewBoxVisible ? 'Hide' : 'Show'} ViewBox`} position='top'>
-          <button onClick={handleToggleViewBox} className={`button--icon icon--xxl `}>
-            {isViewBoxVisible ? <i className='fa-solid fa-eye-slash'></i> : <i className='fa-solid fa-eye'></i>}
+        <Tooltip content={`${isDarkBackground ? 'Light' : 'Dark'} Background`} position="top">
+          <button onClick={handleToggleBackground} className="button--icon icon--xxl">
+            {isDarkBackground ? <i className="fa-solid fa-sun"></i> : <i className="fa-solid fa-moon"></i>}
+          </button>
+        </Tooltip>
+        <Tooltip content={`${isCheckeredBackground ? 'Hide' : 'Show'} Checkered Background`} position="top">
+          <button onClick={handleCheckeredBackground} className="button--icon icon--xxl">
+            {isCheckeredBackground ? <i className="fa-solid fa-border-none"></i> : <i className="fa-solid fa-border-all"></i>}
+          </button>
+        </Tooltip>
+        <Tooltip content={`${isViewBoxVisible ? 'Hide' : 'Show'} ViewBox`} position="top">
+          <button onClick={handleToggleViewBox} className="button--icon icon--xxl">
+            {isViewBoxVisible ? <i className="fa-solid fa-eye-slash"></i> : <i className="fa-solid fa-eye"></i>}
           </button>
         </Tooltip>
       </div>
--- src/features/dashboard/modal/widgets/modalHeader/ModalHeader.tsx
+++ src/features/dashboard/modal/widgets/modalHeader/ModalHeader.tsx
@@ -4,8 +4,8 @@ import { copyToClipboard } from "../../../../../helpers/copyToClipboard";
 const ModalHeader = ({ iconName }: { iconName: string }) => (
     <Tooltip content='Copy Name' position='bottom' flashMessage='Copied!'>
       <button onClick={() => copyToClipboard(iconName)} className='button button--icon'>
-        <p className='icon-title'>{iconName}</p>
         <i className='fa-solid fa-copy text-xl'></i>
+        <p className='icon-title'>{iconName}</p>
       </button>
     </Tooltip>
   );
