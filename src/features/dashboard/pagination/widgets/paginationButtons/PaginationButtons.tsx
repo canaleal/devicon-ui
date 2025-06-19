2883994dc15a0deea2142abd0c9140ef243d7ed1
canaleal <alexcanales766@gmail.com>
2025-06-18 23:52:41 -0400

    Removed react from PaginationButtons

--- src/features/dashboard/pagination/widgets/paginationButtons/PaginationButtons.tsx
+++ src/features/dashboard/pagination/widgets/paginationButtons/PaginationButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getPaginationButtons } from './paginationHelpers'
 import './paginationButtons.css'
 
